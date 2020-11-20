9620450b52b991109f12da2388099d7e7b8ab199
sumilraweng <sumil.jain@raweng.com>
2020-11-20 18:47:44 +0530

    Added readme file and created create- markup.js
    in readme live demo link is added and create-markup.js contain the html markup and in index.html change the h1 tag text to employee cards

---
+++ README.md
@@ -0,0 +1,1 @@
+[LIVE DEMO](https://sumilraweng.github.io/employee-list/)
--- src/emloyee-data.js
+++ src/emloyee-data.js
@@ -1,56 +1,6 @@
 // ------Url of json-----------------
 const url="https://niravkpatel28.github.io/json-data-server/employees/employees.json";
 
-// ------------Creating  the html card container--------------
-//  <body>
-//     <div class="employee-cards">
-//     </div>
-// </body> 
-const employeeCards=document.createElement("div");
-employeeCards.className="employee-cards"
-document.body.appendChild(employeeCards);
-
-
-//createcards function is used to create the html cards again and again
-const createCards=(employeeObj)=>{
-        // creating the card elemnt
-        const card=document.createElement("div");
-        const img=document.createElement("img")
-        const contentSection=document.createElement("div");
-        const name=document.createElement("h3");
-        const hobbies=document.createElement("p");
-        
-        //giving the class to the element
-        card.className="card"
-        contentSection.className="content-setion"   
-        name.className="name"
-
-        //Giving the unique id to the each employee name(means <p id="json.id"></p>)
-        name.id=employeeObj["id"]
-
-        // giving the hierarchy to the elments
-        // <div class="employee-cards">
-        //     <div class="card">
-        //         <img src="" alt="">
-        //         <div class= "content-section">
-        //             <h3 class="name" id="<getting id from json data>">name</h3>
-        //         </div>
-        //     </div>
-        // </div>
-
-        employeeCards.appendChild(card);
-        card.append(img,contentSection);
-        contentSection.appendChild(name);
-
-        //Assing the name from json file to the h3 tag having class "name"
-        name.innerText=employeeObj.firstName+" "+employeeObj.lastName;
-
-        //Assignin the image url from json data 
-        img.src=employeeObj.imageUrl;
-        img.setAttribute("alt","Employee name");
-    
-}
-
 
 
 
